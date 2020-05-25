db9439c3a1586b92ad1e479e46b68be8bc51fab1
yanazPL <jnk.gregorczyk@gmail.com>
2020-05-25 21:26:38 +0200

    minimal changes

--- questions/forms.py
+++ questions/forms.py
@@ -6,8 +6,10 @@ class QuestionForm(ModelForm):
     class Meta:
         model = Question
         fields = ['title', 'content']
+        labels = {'title' : 'Tytuł', 'content' : 'Treść'}
 
 class AnswerForm(ModelForm):
     class Meta:
         model = Answer
         fields = ['content']
+        labels = {'content' : 'Treść'}
--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponseRedirect
 from.forms import QuestionForm, AnswerForm
 from .models import Question
 
@@ -13,13 +13,15 @@ def ask(request):
             question = form.save(commit=False)
             question.author = request.user
             question.save()
-            return redirect("index")
+            # return ("question", question_id=question.pk)
+            return HttpResponseRedirect(reverse('question', args=(question.id,)))
     else:
         form = QuestionForm()
     return render(request, 'questions/ask.html', {'form' : form})
 
 def answer(request):
     # if request.method = "POST":
+    pass
 
 def question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
